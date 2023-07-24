b8c2f6c096ea3057224c4d2ad669a7bd4b82b7ef
Alex <71983118+Aleexs@users.noreply.github.com>
2023-07-24 15:26:41 -0500

    Actualizacion Carta.h
    Agregado funcion para comparar de forma correcta

--- Modelo/Carta.h
+++ Modelo/Carta.h
@@ -9,7 +9,8 @@ public:
     enum class Tipo { FUEGO_, HIELO_, AGUA_, DESCONOCIDO };
     enum class Color { ROJO_, AZUL_, VERDE_, DESCONOCIDO };
 
-    Carta(Tipo tipo, Color color, int numero);
+    Carta(Tipo tipo = Tipo::DESCONOCIDO, Color color = Color::DESCONOCIDO, int numero = 0);
+
 
     Tipo getTipo() const;
     Color getColor() const;
@@ -18,7 +19,10 @@ public:
     int getNumero() const; 
 
     
-    bool operator==(const Carta& otraCarta) const;
+    int comparar(const Carta& otraCarta) const; // Funcion para comparar dos cartas
+    bool operator==(const Carta& otra) const {
+        return tipo_ == otra.tipo_ && color_ == otra.color_ && numero_ == otra.numero_;
+    }
 
 private:
     Tipo tipo_;
