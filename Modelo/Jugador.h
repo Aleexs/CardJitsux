170a0e0fa8f2f184fd87e0b657c3419a46c4bc17
Alex <71983118+Aleexs@users.noreply.github.com>
2023-07-25 14:15:12 -0500

    25/Jugador.h
    Removida la puntuacion por generar errores, falta implementar puntuacion para ranking

--- Modelo/Jugador.h
+++ Modelo/Jugador.h
@@ -2,67 +2,48 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
-#include "carta.h"
 #include "mazo.h"
-#include <vector>
 #include <string>
-#include <fstream>
-#include <sstream>
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <algorithm>
 
 class Jugador {
 public:
-    Jugador(const std::string& nombre, const std::vector<Carta>& mazoOriginal, int puntuacion = 0);
+    Jugador(const std::string& nombre, const Mazo& mazoOriginal);
+    std::string getNombre() const;
 
-    void importarPuntuacion();
-    void exportarPuntuacion() const;
-    int getPuntuacion() const;
-    void actualizarPuntuacion(int puntos);
+    void mezclarCartas();
 
-    std::vector<Carta> obtenerMazoMezclado() const;
+    void mostrarMazoOriginal() const;
+    void mostrarMazoAleatorio() const;
+    std::vector<Carta> obtenerMazoAleatorio() const;
+
     void generarMano();
     void mostrarMano() const;
+    std::vector<Carta> obtenerMano() const;
+
     Carta elegirCarta();
-    const Carta& obtenerCartaElegida() const;
-    const std::string& obtenerNombre() const;
 
-    int enfrentarCartas(Jugador& jugadorContrincante, const Carta& cartaJugador1, const Carta& cartaJugador2);
+    Carta obtenerCartaElegida();
+    void enfrentarCartas(const Carta& cartaJugador1, const Carta& cartaJugador2, Jugador& jugadorContrario);
+    void mostrarEstadoRondaContrario(const Carta& cartaGanadoraContrario);
 
-    const std::vector<Carta>& obtenerCartasGanadas() const;
-    const std::vector<Carta>& obtenerCartasGanadasRonda() const;
+    bool verificarGanadorPartida();
 
-    std::string obtenerGanadorRonda(const Jugador& jugadorContrincante, const Carta& cartaJugador1, const Carta& cartaJugador2) const;
-    std::string obtenerPerdedorRonda(const Jugador& jugadorContrincante, const Carta& cartaJugador1, const Carta& cartaJugador2) const;
-    void mostrarGanadorRonda(const Jugador& jugadorContrincante, const Carta& cartaJugador1, const Carta& cartaJugador2) const;
-    void mostrarPerdedorRonda(const Jugador& jugadorContrincante, const Carta& cartaJugador1, const Carta& cartaJugador2) const;
-
-
-    bool esGanadorPartida() const;
-    const std::string& obtenerNombreGanadorPartida() const;
-    const std::string& obtenerNombrePerdedorPartida() const;
-    void actualizarPuntuacionPartida();
-
-    void aumentarPuntuacion();
-    void reducirPuntuacion();
-
+    
+    const std::vector<Carta>& obtenerCartasGanadoras() const;
+    void mostrarCartasGanadoras() const;
 private:
     std::string nombre_;
-    int puntuacion_;
     std::vector<Carta> mazoOriginal_;
-    std::vector<Carta> mazoMezclado_;
+    std::vector<Carta> mazoAleatorio_;
+        
     std::vector<Carta> mano_;
-    Carta cartaElegida_ = Carta(Carta::Tipo::DESCONOCIDO, Carta::Color::DESCONOCIDO, 0); // Valor predeterminado
 
-    std::vector<Carta> cartasGanadasPropias_;
-    std::vector<Carta> cartasGanadasRonda_;
-
-    void eliminarCartaDeMano(const Carta& carta);
-    void guardarCartaGanadaPropia(const Carta& cartaGanada);
-
-    int importarPuntuacion(const std::string& nombreJugador);
-    void exportarPuntuacion(const std::string& nombreJugador, int puntuacion);
-
-
-
+    std::vector<Carta> cartasGanadoras_;
 };
 
 #endif // JUGADOR_H
